a8997268d6449e7fdbf9e8bc6d4a8ab9f1db7ae9
Louis Turpault <louis.turpault@gmail.com>
2026-03-07 11:49:17 -0500

    off by one error

--- spawn_tmux.sh
+++ spawn_tmux.sh
@@ -9,22 +9,22 @@ fi
 # create new session detached
 tmux new-session -d -s "$SESSION" -n "inference testing"
 
-tmux split-window -h -t "$SESSION:1"
-tmux split-window -v -t "$SESSION:1.1"
+tmux split-window -h -t "$SESSION:0"
+tmux split-window -v -t "$SESSION:0.1"
 # window 1: editor
-tmux send-keys -t "$SESSION:1.0" "cd /home/lrima/Documents/prog/inference/Metric3D" C-m
-tmux send-keys -t "$SESSION:1.1" "cd /home/lrima/Documents/prog/inference/Metric3D" C-m
-tmux send-keys -t "$SESSION:1.2" "cd /home/lrima/Documents/prog/inference/Metric3D" C-m
+tmux send-keys -t "$SESSION:0.0" "cd /home/lrima/Documents/prog/inference/Metric3D" C-m
+tmux send-keys -t "$SESSION:0.1" "cd /home/lrima/Documents/prog/inference/Metric3D" C-m
+tmux send-keys -t "$SESSION:0.2" "cd /home/lrima/Documents/prog/inference/Metric3D" C-m
 
-tmux send-keys -t "$SESSION:1.0" "uv run inference_v2.py /mnt/harddrive/training_data/nuscenes_data /mnt/harddrive/training_data" C-m
-tmux send-keys -t "$SESSION:1.1" "glances" C-m
-tmux send-keys -t "$SESSION:1.2" "nvtop" C-m
+tmux send-keys -t "$SESSION:0.0" "uv run inference_v2.py /mnt/harddrive/training_data/nuscenes_data /mnt/harddrive/training_data" C-m
+tmux send-keys -t "$SESSION:0.1" "glances" C-m
+tmux send-keys -t "$SESSION:0.2" "nvtop" C-m
 
 # window 2: server + logs (split vertically)
 tmux new-window -t "$SESSION" -n "shell"
-tmux send-keys -t "$SESSION:2" "cd /home/lrima/Documents/prog/inference/Metric3D" C-m
+tmux send-keys -t "$SESSION:1" "cd /home/lrima/Documents/prog/inference/Metric3D" C-m
 
-tmux select-window -t "$SESSION:1"
+tmux select-window -t "$SESSION:0"
 
 tmux attach -t "$SESSION"
 
