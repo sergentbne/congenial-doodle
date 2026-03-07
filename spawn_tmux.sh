122b4ce163b0f9793c63f31b6dbfce93c93a597c
Louis Turpault <louis.turpault@gmail.com>
2026-03-07 11:42:24 -0500

    added convenience script tmux

---
+++ spawn_tmux.sh
@@ -0,0 +1,30 @@
+#!/usr/bin/env bash
+SESSION="inference_model"  # session name
+
+# if session exists, attach
+if tmux has-session -t "$SESSION" 2>/dev/null; then
+  tmux kill-session -t "$SESSION"
+fi
+
+# create new session detached
+tmux new-session -d -s "$SESSION" -n "inference testing"
+
+tmux split-window -h -t "$SESSION:1"
+tmux split-window -v -t "$SESSION:1.2"
+# window 1: editor
+tmux send-keys -t "$SESSION:1.1" "cd /home/lrima/Documents/prog/inference/Metric3D" C-m
+tmux send-keys -t "$SESSION:1.2" "cd /home/lrima/Documents/prog/inference/Metric3D" C-m
+tmux send-keys -t "$SESSION:1.3" "cd /home/lrima/Documents/prog/inference/Metric3D" C-m
+
+tmux send-keys -t "$SESSION:1.1" "uv run inference_v2.py /mnt/harddrive/training_data/nuscenes_data /mnt/harddrive/training_data" C-m
+tmux send-keys -t "$SESSION:1.2" "glances" C-m
+tmux send-keys -t "$SESSION:1.3" "nvtop" C-m
+
+# window 2: server + logs (split vertically)
+tmux new-window -t "$SESSION" -n "shell"
+tmux send-keys -t "$SESSION:2" "cd /home/lrima/Documents/prog/inference/Metric3D" C-m
+
+tmux select-window -t "$SESSION:1"
+
+tmux attach -t "$SESSION"
+
